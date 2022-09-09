0796875dc29b738ba7d2407d765b172a33fbdd26
asif a owadud <asifaowadud@gmail.com>
2022-09-10 01:00:36 +0600

    ready to deploy

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
-const DBConnect = require("./utils/dbConnect");
+// const DBConnect = require("./utils/dbConnect");
 
 const app = require("./app");
 
 // database connection
-DBConnect();
+mongoose.connect(process.env.DB_URI).then(() => {
+  console.log("Database connected".green);
+});
+
 
 // server
 const port = process.env.PORT || 8080;
